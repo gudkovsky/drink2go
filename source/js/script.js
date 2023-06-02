57775f225b9effc41ebef73dd84ceab8bcd553f8
Gudkov <gudkov.id@mail.ru>
2023-06-02 17:03:37 +0300

    feat: add swiper slider

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,20 @@
-/* в этот файл добавляет скрипты*/
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+//   navigation: {
+//   nextEl: ".promo__swiper-button--left",
+//   prevEl: ".promo__swiper-button--right",
+// },
+});
+const prevBtn = document.querySelector('.promo__swiper-button--left');
+const nextBtn = document.querySelector('.promo__swiper-button--right');
+
+prevBtn.addEventListener('click', () => {
+  swiper.slidePrev();
+});
+
+nextBtn.addEventListener('click', () => {
+  swiper.slideNext();
+});
