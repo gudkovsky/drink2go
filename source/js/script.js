4f46b0e97a119b061a6c0b77bc9367374de88270
Gudkov <gudkov.id@mail.ru>
2023-06-02 21:50:11 +0300

    feat: add menu toggle

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,22 @@
-var swiper = new Swiper(".mySwiper", {
+// NAVIGATION
+
+const headerWrapper = document.querySelector('.header__wrapper');
+const headerNav = document.querySelector('.header__nav');
+
+headerWrapper.classList.remove('no-js');
+headerNav.classList.remove('no-js');
+headerNav.classList.remove('active');
+
+const burgerButton = document.querySelector('.user-box__menu');
+
+burgerButton.addEventListener('click', () => {
+  headerNav.classList.toggle('active');
+  burgerButton.classList.toggle('active');
+})
+
+// SLIDER
+
+let swiper = new Swiper(".mySwiper", {
   loop: true,
   pagination: {
     el: ".swiper-pagination",
@@ -18,3 +36,32 @@ prevBtn.addEventListener('click', () => {
 nextBtn.addEventListener('click', () => {
   swiper.slideNext();
 });
+
+// LEAFLET
+
+let map = L.map('map')
+.setView([59.96831, 30.31748], 16);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data © OpenStreetMap contributors',
+    maxZoom: 19
+}).addTo(map);
+
+
+const mainPinIcon = L.icon({
+  iconUrl: '../assets/img/icons/map-marker.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+
+const mainPinMarker = L.marker(
+  {
+    lat: 59.96831,
+    lng: 30.31748,
+  },
+  {
+    draggable: false,
+    icon: mainPinIcon,
+  },
+);
+
+mainPinMarker.addTo(map);
