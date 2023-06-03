56a939e2159eba27c0d47db2350b6086aae0d518
Gudkov <gudkov.id@mail.ru>
2023-06-03 18:47:27 +0300

    feat: add tablet adaptive

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,7 @@
 
 const headerWrapper = document.querySelector('.header__wrapper');
 const headerNav = document.querySelector('.header__nav');
+const navItem = document.querySelectorAll('.nav__item')
 
 headerWrapper.classList.remove('no-js');
 headerNav.classList.remove('no-js');
@@ -14,9 +15,17 @@ burgerButton.addEventListener('click', () => {
   burgerButton.classList.toggle('active');
 })
 
+navItem.forEach((el) => {
+  el.addEventListener('click', () => {
+    headerNav.classList.remove('active');
+    burgerButton.classList.remove('active');
+  })
+})
+
+
 // SLIDER
 
-let swiper = new Swiper(".mySwiper", {
+let swiper = new Swiper(".swiper", {
   loop: true,
   pagination: {
     el: ".swiper-pagination",
@@ -65,3 +74,87 @@ const mainPinMarker = L.marker(
 );
 
 mainPinMarker.addTo(map);
+
+// CUSTOM SELECT
+
+const selectPick = document.querySelector('.select__pick')
+const selectList = document.querySelector('.select__list')
+const selectItem = document.querySelectorAll('.select__item')
+
+selectPick.addEventListener('click', () => {
+  selectList.classList.toggle('dropdown-active')
+  selectPick.classList.toggle('active')
+})
+
+selectItem.forEach((el) => {
+  if (el.innerText == selectPick.innerText) {
+    el.classList.add('chosen')
+  };
+
+  el.addEventListener('click', () => {
+    selectList.classList.toggle('dropdown-active');
+    selectPick.classList.toggle('active');
+
+    console.log('выбран тип сортировки:', el.innerText);
+  })
+})
+
+// CUSTOM PAGINATION
+
+const pageLinks = document.querySelectorAll('.pagination__page');
+const prevPageButton = document.querySelector('.pagination__button--prev');
+const nextPageButton = document.querySelector('.pagination__button--next');
+
+const handleActivePage = function () {
+  if (pageLinks[0].classList.contains('pagination__page--current')) {
+    prevPageButton.classList.add('js-hidden')
+  } else {prevPageButton.classList.remove('js-hidden')}
+  if (pageLinks[pageLinks.length-1 ].classList.contains('pagination__page--current')) {
+    nextPageButton.classList.add('js-hidden')
+  } else {nextPageButton.classList.remove('js-hidden')}
+};
+
+pageLinks.forEach(link => {
+  link.addEventListener('click', (evt) => {
+    evt.preventDefault()
+    pageLinks.forEach(el => el.classList.remove('pagination__page--current'))
+    link.classList.add('pagination__page--current')
+    handleActivePage();
+  })
+})
+
+prevPageButton.addEventListener('click', () => {
+  let index = -1;
+for (let i = 0; i < pageLinks.length; i++) {
+  if (pageLinks[i].classList.contains('pagination__page--current')) {
+    index = i;
+    break;
+  }
+}
+
+if (index !== -1) {
+  pageLinks[index].classList.remove('pagination__page--current');
+  pageLinks[index-1].classList.add('pagination__page--current');
+  handleActivePage();
+} else {
+  return
+}
+})
+
+nextPageButton.addEventListener('click', () => {
+  let index = -1;
+for (let i = 0; i < pageLinks.length; i++) {
+  if (pageLinks[i].classList.contains('pagination__page--current')) {
+    index = i;
+    break;
+  }
+}
+
+if (index !== -1) {
+  pageLinks[index].classList.remove('pagination__page--current');
+  pageLinks[index+1].classList.add('pagination__page--current');
+  handleActivePage();
+} else {
+  return
+}
+})
